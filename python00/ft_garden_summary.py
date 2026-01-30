3dcaee03ffe6c0b40403beec5825d0a32002f25e
gtomas <gtomas@student.42luxembourg.lu>
2026-01-30 10:26:08 +0100

    did module 00

--- python00/ft_garden_summary.py
+++ python00/ft_garden_summary.py
@@ -1,6 +1,6 @@
 def ft_garden_summary():
 	name = input("Enter garden name: ")
-	nb_plants = int(input("Enter number of plants: "))
+	nb_plants = (input("Enter number of plants: "))
 	print("Garden:", name)
 	print("Plants:", nb_plants)
 	print("Status: Growing well!")
