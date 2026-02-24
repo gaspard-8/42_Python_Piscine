c7298b64bbb90bd56aa9388e6a3a4b759b7bd5c8
gtomas <gtomas@student.42luxembourg.lu>
2026-02-24 13:25:20 +0100

    .

--- python03/ex5/ft_data_stream.py
+++ python03/ex5/ft_data_stream.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
-# from typing import Generator
+from typing import Generator
+from timeit import default_timer as timer
 
 
 def cycle(iterate: list):
@@ -35,17 +36,57 @@ def game_event(size: int):
     print(f"Treasure events: {tevent_count}")
     print(f"Level-up events: {lvl_event_count}")
     print()
-    print(f"Memory usage: onstant ")
+
+
+def fibonacci() -> Generator[int, None, None]:
+    a = 0
+    b = 1
+    while True:
+        yield a
+        c = a + b
+        a = b
+        b = c
+
+
+def is_prime(a: int) -> bool:
+    for i in range(2, a // 2 + 1):
+        if a % i == 0:
+            return False
+    return True
+
+
+def prime() -> Generator[int, None, None]:
+    a = 2
+    while True:
+        yield a
+        a += 1
+        while (not is_prime(a)):
+            a += 1
+
 
 def main():
     print("=== Game data Stream Processor ===")
     print()
     nb_of_events = 500
     print(f"Processing {nb_of_events} events...")
+    start = timer()
     game_event(nb_of_events)
-
-    print("=== Stream analytics ===")
-    print(f"Total events processed : {nb_of_events}")
+    end = timer()
+    time_taken = end - start
+    print("Memory usage: Constant (streaming)")
+    print(f"Processing time: {time_taken:.2} seconds")
+    print()
+    print("=== Generator Demonstration ===")
+    fibo = fibonacci()
+    prime_num = prime()
+    print("Fibonacci sequence (first 10): ", end="")
+    for i in range(9):
+        print(next(fibo), end=", ")
+    print((next(fibo)))
+    print("Prime numbers (first 10): ", end="")
+    for i in range(9):
+        print(next(prime_num), end=", ")
+    print((next(prime_num)))
 
 
 if __name__ == "__main__":
